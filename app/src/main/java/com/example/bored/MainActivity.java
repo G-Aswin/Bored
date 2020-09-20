c168518a8ced5d82088f478aa76d50ceb9fdb8e5
G-Aswin <aswin.g.rns@gmail.com>
2020-09-20 15:04:23 +0530

    Final color discrepancy fixed at the startup screen.

--- app/src/main/java/com/example/bored/MainActivity.java
+++ app/src/main/java/com/example/bored/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.bored;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import com.google.android.material.button.MaterialButton;
@@ -17,6 +20,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Window window = MainActivity.this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(ContextCompat.getColor(MainActivity.this, R.color.colorPrimary));
     }
 
     public void StartMainScreen(View view) {
